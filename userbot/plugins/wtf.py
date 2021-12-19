aef8dd0913d3b080477f76cdd4e612652448e49e
Psycho_hacker <69103277+hopepsycho@users.noreply.github.com>
2021-12-19 15:20:48 +0530

    Update wtf.py

--- userbot/plugins/wtf.py
+++ userbot/plugins/wtf.py
@@ -22,7 +22,7 @@ async def _(event):
         "What The F",
         "What The Fa",
         "What The Fak Brah",
-        "[What The Fakk Brah](https://telegra.ph//file/f3b760e4a99340d331f9b.jpg)",
+        "[What The Fakk Brah](https://telegra.ph//file/f3b760e4a99340d351f9b.jpg)",
     ]
 
     for i in animation_ttl:
