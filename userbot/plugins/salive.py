5d593e041daba29f7677dcf4241204e273361613
Psycho_hacker <69103277+hopepsycho@users.noreply.github.com>
2021-12-19 20:59:10 +0530

    Update salive.py

--- userbot/plugins/salive.py
+++ userbot/plugins/salive.py
@@ -38,7 +38,7 @@ def get_readable_time(seconds: int) -> str:
 uptime = get_readable_time((time.time() - Lastupdate))
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = Var.ALIVE_IMAGE
-pm_caption = "➥ **𝔅𝔩𝔞𝔠𝔨 𝔏𝔦𝔤𝔥𝔱𝔫𝔦𝔫𝔤 IS:** `ONLINE`\n\n"
+pm_caption = "➥ **𝔅𝔩𝔞𝔠𝔨 𝔲𝔰𝔢𝔯𝔟𝔬𝔱 IS:** `ONLINE`\n\n"
 pm_caption += "➥ **SYSTEMS STATS**\n"
 pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
 pm_caption += "➥ **Python:** `3.7.4` \n"
@@ -49,9 +49,9 @@ pm_caption += f"➥ **Version** : `{telever}`\n"
 pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
 pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
 pm_caption += "➥ **License** : [GNU General Public License v3.0](https://github.com/Anmol-dot283/Black-Lightning)\n"
-pm_caption += "➥ **Copyright** : By [KeinShin@Github](GitHub.com/StarkGang)\n"
+pm_caption += "➥ **Copyright** : By [hopepsycho@Github](GitHub.com/hopepsycho)\n"
 pm_caption += "➥ **Check Stats By Doing** `.stat`. \n\n"
-pm_caption += "[🇮🇳 Deploy 𝔅𝔩𝔞𝔠𝔨 𝔏𝔦𝔤𝔥𝔱𝔫𝔦𝔫𝔤Userbot 🇮🇳](https://telegra.ph/FRIDAY-06-15)"
+pm_caption += "[🇮🇳 Deploy 𝔅𝔩𝔞𝔠𝔨 𝔘𝔰𝔢𝔯𝔟𝔬𝔱 Userbot 🇮🇳](https://telegra.ph/)"
 
 
 @borg.on(lightning_cmd(pattern=r"fralive"))
