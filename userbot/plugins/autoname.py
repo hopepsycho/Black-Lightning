da960a46b360a22cfd2aaece7c8795e08bdbf9de
Psycho_hacker <69103277+hopepsycho@users.noreply.github.com>
2021-12-23 10:27:27 +0530

    Update autoname.py

--- userbot/plugins/autoname.py
+++ userbot/plugins/autoname.py
@@ -10,7 +10,7 @@ from uniborg.util import lightning_cmd
 from userbot import ALIVE_NAME, CMD_HELP
 
 DEL_TIME_OUT = 60
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Lightning"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "userbotz"
 
 
 @borg.on(lightning_cmd(pattern="autoname"))  # pylint:disable=E0602
